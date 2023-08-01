d707a7b046887df742042de888f3d271ac83ffbf
AurelienDeragne <aderagne@gmail.com>
2023-08-01 15:54:52 +0200

    Correction bug email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !empty($_POST['telephone']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
@@ -10,10 +10,10 @@ $email = strip_tags(htmlspecialchars($_POST['email']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
-$to = "psi.climat@gmail.com";
+$to = "contact@puzzleclimat.org";
 $subject = "Le Puzzle Climat - message de $name";
 $body = "Nouveau message reçu depuis le site web du Puzzle Climat, de la part de $name - $email\n\n$message";
-$header = "From: psi.climat@gmail.com\n";
+$header = "From: contact@puzzleclimat.org\n";
 $header .= "Reply-To: $email";
 
 if(!mail($to, $subject, $body, $header))
